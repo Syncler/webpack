7244e7401e70b99518afb28aca445e55fcd048a4
Syncler <syncler_jssa@yahoo.com.br>
2019-11-27 18:41:23 -0300

    alterações feitas no arquivo webpack.js

---
+++ webpack-config/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Button from './Button'
+/*import './App.css';*/
+
+{/*function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/*<img src={logo} className="App-logo" alt="logo" />*/}
+const App = () => {
+
+  return (
+    <div>
+      <p>Digital Innovation One</p>
+      <p>Olá, seja bem vindo ao mundo React</p>
+      <p>Testando webpack 4</p>
+      <p>Isso ai, aprendendo aos poucos</p>
+      <Button />
+    </div>
+  );
+};
+
+export default App;
---
+++ webpack-config/src/Button.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+const Button = () => (
+    <button>Sou um Botão</button>
+)
+export default Button;
